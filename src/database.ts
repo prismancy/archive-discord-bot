f11376ab87df5b2da32758d2ae1b0a77553f23f5
iz7n <48165199+iz7n@users.noreply.github.com>
2024-11-18 00:51:52 -0500

    fix(drizzle): set snake_case

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
 const sqlite = new Database(Bun.env.DATABASE_URL || "");
-const db = drizzle(sqlite, { schema });
+const db = drizzle(sqlite, { schema, casing: "snake_case" });
 export default db;
 
 export * from "drizzle-orm";
